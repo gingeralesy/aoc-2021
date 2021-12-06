5f1c254615e3ec974d4d2213eeba8b3ddeb3dd1f
Janne Pakarinen <gingeralesy@gmail.com>
2021-12-06 19:41:55 +0200

    Day 5, part 2

--- day5.lisp
+++ day5.lisp
@@ -101,3 +101,60 @@ Consider only horizontal and vertical lines. At how many points do at least two 
             finally (return danger)))))
 
 ;; Answer: 5306
+
+#|
+--- Part Two ---
+
+Unfortunately, considering only horizontal and vertical lines doesn't give you the full picture; you
+need to also consider diagonal lines.
+
+Because of the limits of the hydrothermal vent mapping system, the lines in your list will only ever
+be horizontal, vertical, or a diagonal line at exactly 45 degrees. In other words:
+
+- An entry like 1,1 -> 3,3 covers points 1,1, 2,2, and 3,3.
+- An entry like 9,7 -> 7,9 covers points 9,7, 8,8, and 7,9.
+
+Considering all lines from the above example would now produce the following diagram:
+
+1.1....11.
+.111...2..
+..2.1.111.
+...1.2.2..
+.112313211
+...1.2....
+..1...1...
+.1.....1..
+1.......1.
+222111....
+
+You still need to determine the number of points where at least two lines overlap. In the above
+example, this is still anywhere in the diagram with a 2 or larger - now a total of 12 points.
+
+Consider all of the lines. At how many points do at least two lines overlap?
+|#
+
+(defun d5p2 ()
+  (multiple-value-bind (coordinates width height)
+      (d5-data)
+    (let ((diagram (make-array (list width height) :element-type 'integer :initial-element 0)))
+      (loop for ((x1 . y1) (x2 . y2)) in coordinates
+            do (cond
+                 ((= y1 y2) ;; Horizontal.
+                  (loop for x from (min x1 x2) to (max x1 x2)
+                        do (incf (aref diagram y1 x))))
+                 ((= x1 x2) ;; Vertical.
+                  (loop for y from (min y1 y2) to (max y1 y2)
+                        do (incf (aref diagram y x1))))
+                 (T ;; Diagonal.
+                  (loop for x = x1 then (if (< x1 x2) (1+ x) (1- x))
+                        for y = y1 then (if (< y1 y2) (1+ y) (1- y))
+                        while (and (<= x (max x1 x2)) (<= (min x1 x2) x))
+                        do (incf (aref diagram y x))))))
+      (loop with danger = 0
+            for y from 0 below height
+            do (loop for x from 0 below width
+                     when (< 1 (aref diagram y x))
+                     do (incf danger))
+            finally (return danger)))))
+
+;; Answer: 17787
