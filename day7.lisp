2443ec6ef79bf0b1bf69e3f8c6e3e5f6d9935df3
Janne Pakarinen <gingeralesy@gmail.com>
2021-12-07 10:49:48 +0200

    Day 7, part 2

--- day7.lisp
+++ day7.lisp
@@ -87,3 +87,54 @@ much fuel must they spend to align to that position?
             finally (return (values min index))))))
 
 ;; Answer: 333755
+
+#|
+--- Part Two ---
+The crabs don't seem interested in your proposed solution. Perhaps you misunderstand crab
+engineering?
+
+As it turns out, crab submarine engines don't burn fuel at a constant rate. Instead, each change of
+1 step in horizontal position costs 1 more unit of fuel than the last: the first step costs 1, the
+second step costs 2, the third step costs 3, and so on.
+
+As each crab moves, moving further becomes more expensive. This changes the best horizontal position
+to align them all on; in the example above, this becomes 5:
+
+- Move from 16 to 5: 66 fuel
+- Move from 1 to 5: 10 fuel
+- Move from 2 to 5: 6 fuel
+- Move from 0 to 5: 15 fuel
+- Move from 4 to 5: 1 fuel
+- Move from 2 to 5: 6 fuel
+- Move from 7 to 5: 3 fuel
+- Move from 1 to 5: 10 fuel
+- Move from 2 to 5: 6 fuel
+- Move from 14 to 5: 45 fuel
+
+This costs a total of 168 fuel. This is the new cheapest possible outcome; the old alignment
+position (2) now costs 206 fuel instead.
+
+Determine the horizontal position that the crabs can align to using the least fuel possible so they
+can make you an escape route! How much fuel must they spend to align to that position?
+|#
+
+(defun d7p2 ()
+  (multiple-value-bind (positions max-position)
+      (d7-data)
+    (let ((fuel-use (make-array (1+ max-position) :initial-element 0 :element-type 'integer)))
+      ;; TODO: This is rather slow. Is there a better way to get the values?
+      (loop for crab in positions
+            do (loop for n from 0 to max-position
+                     for distance = (abs (- n crab))
+                     for cost = (loop for m from 0 to distance summing m)
+                     do (incf (aref fuel-use n) cost)))
+      (loop with min = NIL
+            with index = NIL
+            for fuel across fuel-use
+            for n from 0
+            do (when (or (null min) (< fuel min))
+                 (setf min fuel
+                       index n))
+            finally (return (values min index))))))
+
+;; Answer: 94017638
