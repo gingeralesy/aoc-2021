bf631c6f7d6d5f19a5391d0022c05309c2bcfbf6
Janne Pakarinen <gingeralesy@gmail.com>
2021-12-16 17:03:00 +0200

    Day 16, part 2. Well that was easy.

--- day16.lisp
+++ day16.lisp
@@ -160,6 +160,37 @@ the version numbers in all packets?
   (literal 0 :type (unsigned-byte 64))
   (sub-packets NIL :type (or null list)))
 
+(defmethod d16-packet-value ((packet d16-packet))
+  (ecase (d16-packet-type packet)
+    (0 ;; Sum.
+     (loop for sub in (d16-packet-sub-packets packet)
+           summing (d16-packet-value sub)))
+    (1 ;; Product.
+     (loop with product = 1
+           for sub in (d16-packet-sub-packets packet)
+           do (setf product (* product (d16-packet-value sub)))
+           finally (return product)))
+    (2 ;; Minimum.
+     (loop for sub in (d16-packet-sub-packets packet)
+           minimizing (d16-packet-value sub)))
+    (3 ;; Maximum.
+     (loop for sub in (d16-packet-sub-packets packet)
+           maximizing (d16-packet-value sub)))
+    (4 ;; Literal.
+     (d16-packet-literal packet))
+    (5 ;; Greater than.
+     (let ((subs (d16-packet-sub-packets packet)))
+       (when (third subs) (error "Too many sub-packets."))
+       (if (< (d16-packet-value (second subs)) (d16-packet-value (first subs))) 1 0)))
+    (6 ;; Less than.
+     (let ((subs (d16-packet-sub-packets packet)))
+       (when (third subs) (error "Too many sub-packets."))
+       (if (< (d16-packet-value (first subs)) (d16-packet-value (second subs))) 1 0)))
+    (7 ;; Equal to.
+     (let ((subs (d16-packet-sub-packets packet)))
+       (when (third subs) (error "Too many sub-packets."))
+       (if (= (d16-packet-value (first subs)) (d16-packet-value (second subs))) 1 0)))))
+
 (defun d16-get-packet (nibbles &optional (offset 0) max-bits)
   (labels ((bits (count offset)
              "Finds the wanted bits as an integer.
@@ -270,3 +301,53 @@ return - 0 for number of bits, 1 for number of sub-packets"
                      do (inc-count sub))))
       (inc-count (d16-get-packet (d16-data))))
     version-count))
+
+#|
+--- Part Two ---
+
+Now that you have the structure of your transmission decoded, you can calculate the value of the
+expression it represents.
+
+Literal values (type ID 4) represent a single number as described above. The remaining type IDs are
+more interesting:
+
+- Packets with type ID 0 are sum packets - their value is the sum of the values of their
+  sub-packets. If they only have a single sub-packet, their value is the value of the sub-packet.
+- Packets with type ID 1 are product packets - their value is the result of multiplying together the
+  values of their sub-packets. If they only have a single sub-packet, their value is the value of
+  the sub-packet.
+- Packets with type ID 2 are minimum packets - their value is the minimum of the values of their
+  sub-packets.
+- Packets with type ID 3 are maximum packets - their value is the maximum of the values of their
+  sub-packets.
+- Packets with type ID 5 are greater than packets - their value is 1 if the value of the first
+  sub-packet is greater than the value of the second sub-packet; otherwise, their value is 0. These
+  packets always have exactly two sub-packets.
+- Packets with type ID 6 are less than packets - their value is 1 if the value of the first
+  sub-packet is less than the value of the second sub-packet; otherwise, their value is 0. These
+  packets always have exactly two sub-packets.
+- Packets with type ID 7 are equal to packets - their value is 1 if the value of the first
+  sub-packet is equal to the value of the second sub-packet; otherwise, their value is 0. These
+  packets always have exactly two sub-packets.
+
+Using these rules, you can now work out the value of the outermost packet in your BITS transmission.
+
+For example:
+
+- C200B40A82 finds the sum of 1 and 2, resulting in the value 3.
+- 04005AC33890 finds the product of 6 and 9, resulting in the value 54.
+- 880086C3E88112 finds the minimum of 7, 8, and 9, resulting in the value 7.
+- CE00C43D881120 finds the maximum of 7, 8, and 9, resulting in the value 9.
+- D8005AC2A8F0 produces 1, because 5 is less than 15.
+- F600BC2D8F produces 0, because 5 is not greater than 15.
+- 9C005AC2F8F0 produces 0, because 5 is not equal to 15.
+- 9C0141080250320F1802104A08 produces 1, because 1 + 3 = 2 * 2.
+
+What do you get if you evaluate the expression represented by your hexadecimal-encoded BITS transmission?
+|#
+
+(defun d16p2 ()
+  (let ((root (d16-get-packet (d16-data))))
+    (d16-packet-value root)))
+
+;; Answer: 194435634456
